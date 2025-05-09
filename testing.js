00ba939f664e9c2d338acfd5eaf436c070b00480
Tri <trto@ucsd.edu>
2025-05-09 16:01:19 -0700

    testing for yml

---
+++ testing.js
@@ -0,0 +1,13 @@
+/**
+ * Function prints the inputted string n many times
+ * @param {string} something - the color of the car
+ * @param {int} n - how many times
+ *
+*/
+function printSomething (something, n) {
+    for (let i = 1; i<= n; i++) {
+        console.log (something);
+    }
+}
+
+printSomething ("tri", 2);
---
+++ testing2.js
@@ -0,0 +1,10 @@
+/**
+ * Function prints n squared
+ * @param {int} num - the number to be squared
+ *
+*/
+function number2 (num) {
+    console.log (num*num);
+}
+
+number2 (4);
